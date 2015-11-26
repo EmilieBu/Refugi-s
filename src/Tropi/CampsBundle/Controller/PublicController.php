7c6f4fa26e5aaf1e64d3ddb9843bf8a03965ce50
Vincent <giardina.v@gmail.com>
2015-11-26 17:30:33 +0100

    Avancé de la page index / formulaire recherche
    #twig #integration #caAvance

--- src/Tropi/CampsBundle/Controller/PublicController.php
+++ src/Tropi/CampsBundle/Controller/PublicController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Tropi\CampsBundle\Form\RefugieType;
 use Tropi\CampsBundle\Entity\Refugie;
 
-class DefaultController extends Controller
+class PublicController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
         $refugie = new Refugie();
         $form = $this->createForm(new RefugieType(), $refugie);
         
-        return $this->render('TropiCampsBundle:Default:index.html.twig', array('name' => $name, 'form' => $form->createView()));
+        return $this->render('TropiCampsBundle:Public:index.html.twig', array('form' => $form->createView()));
     }
 }
